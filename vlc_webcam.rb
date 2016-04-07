6f0f4bff6c15e0a649d5a418305d821c8c8bb88a
Ira Miller <imiller1985@gmail.com>
2016-04-07 14:22:03 -0700

    cleaned up code

--- vlc_webcam.rb
+++ vlc_webcam.rb
@@ -1,8 +1,9 @@
 require 'open-uri'
 require 'rmagick'
 
-gif_name = "VLC"
-directory_name = "VLSC_cam1_archive"
+# enter the gif name and the subdirectory name where images are stored.
+gif_name = ""
+directory_name = ""
 
 
 def images_to_gif(directory_name, gif_name)
@@ -10,12 +11,12 @@ def images_to_gif(directory_name, gif_name)
   sequence = Magick::ImageList.new
   counter = 1
   Dir.glob("#{directory_name}/*.jpg").each do |image|
-    # If you don't want every image run the if statement below. Currently set up
-    # to run the first image and every 5th image after that (1, 6, 11, ..)
-    # If you want to run every 3rd image you would change counter % 5 to counter % 3
+# If you don't want every image run the if statement below. Currently set up
+# to run the first image and every 5th image after that (1, 6, 11, ..)
+# If you want to run every 3rd image you would change counter % 5 to counter % 3
     if counter == 1 || counter % 5 == 1
       sequence << Magick::Image.read("#{image}").first
-      # Sets the delay between images. Lower number = less delay between image change
+# Sets the delay between images. Lower number = less delay between image change
       sequence.cur_image.delay = 10
     end
     counter += 1
@@ -23,4 +24,3 @@ def images_to_gif(directory_name, gif_name)
   sequence.write("#{gif_name}_timelapse.gif")
 end
 
-images_to_gif(directory_name, gif_name)
--- webcam.rb
+++ webcam.rb
@@ -2,52 +2,47 @@ require 'open-uri'
 require 'rmagick'
 
 image_url = "http://www.nps.gov/webcams-olym/current_ridgecam.jpg"
-# directory_name = 'hurricane_ridge_images'
-gif_name = "VLC"
-directory_name = "VLSC_cam1_archive"
+directory_name = 'hurricane_ridge_images'
+gif_name = "hurricane_ridge"
 
-# Dir.mkdir(directory_name) unless File.exists?(directory_name)
 
-# start_time = Time.now.to_i
-# hours_of_runtime = 5
-# run_time = 3600 * hours_of_runtime
-# finish_time = start_time + run_time
-# sleep_period = 600
+Dir.mkdir(directory_name) unless File.exists?(directory_name)
 
-# def image_downloader(finish_time, directory_name, image_url, sleep_period)
-#   until Time.now.to_i >= finish_time
-#     begin
-#       true if open(image_url)
-#       open(image_url) do |f|
-#         File.open("#{directory_name}/#{Time.now.to_i}.jpg","w") do |file|
-#           file.puts f.read
-#           puts "saved image at #{Time.now}, script will end at #{Time.at(finish_time)}"
-#           sleep sleep_period
-#         end
-#       end
-#     rescue
-#       false
-#       puts "Can't connect at #{Time.now}, trying again in 5 seconds"
-#       sleep 5
-#     end
-#   end
-# end
+start_time = Time.now.to_i
+hours_of_runtime = 5
+run_time = 3600 * hours_of_runtime
+finish_time = start_time + run_time
+sleep_period = 600
+
+def image_downloader(finish_time, directory_name, image_url, sleep_period)
+  until Time.now.to_i >= finish_time
+    begin
+      true if open(image_url)
+      open(image_url) do |f|
+        File.open("#{directory_name}/#{Time.now.to_i}.jpg","w") do |file|
+          file.puts f.read
+          puts "saved image at #{Time.now}, script will end at #{Time.at(finish_time)}"
+          sleep sleep_period
+        end
+      end
+    rescue
+      false
+      puts "Can't connect at #{Time.now}, trying again in 5 seconds"
+      sleep 5
+    end
+  end
+end
 
 
 def images_to_gif(directory_name, gif_name)
   puts "converting to gif"
   sequence = Magick::ImageList.new
-  counter = 1
   Dir.glob("#{directory_name}/*.jpg").each do |image|
-    if counter == 1 || counter % 5 == 1
       sequence << Magick::Image.read("#{image}").first
+# Sets the delay between images. Lower number = less delay between image change
       sequence.cur_image.delay = 10
     end
-    counter += 1
   end
   sequence.write("#{gif_name}_timelapse.gif")
 end
 
-# deletes_photos(directory_name)
-# image_downloader(finish_time, directory_name, image_url, sleep_period)
-images_to_gif(directory_name, gif_name)
